2ca9bdb72d47e245c30b42d3919d28b995373be2
SML028 <goruvenkat992gmail.com>
2025-12-13 07:34:03 +0530

    concertion

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+person1=Trump
+person2=mausk
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person, I am fine. How are you doing"
+echo "$person: I am fine too. What's up?"
+echo "$person2:: Nohting, just going to Mars now, will you come?"
+echo "$person:: Sorry, you carry on! I will come once you come back"
